beda41271e4473c26321b608e543ea1b247f2013
PikachuHy <2931408816@qq.com>
2018-05-24 22:06:55 +0800

    update model test code

--- xsnet/xs_model_test.py
+++ xsnet/xs_model_test.py
@@ -1,3 +1,13 @@
+import numpy as np
+import cupy
+import chainer
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
 from chainer import datasets, iterators, optimizers, serializers, config
 from model import XSNet, Classifier
 from train import handle_data, load_midi_snippet, convert
@@ -39,9 +49,25 @@ def my_test_2():
     n_rhythm = len(target_midi_ids)
     print('rhythm: {}'.format(n_rhythm))
     model = XSNet(3, 54, n_rhythm, 1024)
-    serializers.load_npz('result/snapshot_iter_27375.new', model)
+    npz_path = 'result/model_epoch-80'
+    serializers.load_npz(npz_path, model)
+    chainer.backends.cuda.get_device_from_id(0).use()
+    model.to_gpu()
+
     # serializers.load_npz('result/snapshot_iter_239', model)
     config.train = False
+    device = 0
+    def to_gpu(batch):
+            xp = cuda.cupy.get_array_module(*batch)
+            concat = xp.concatenate(batch, axis=0)
+            sections = np.cumsum([len(x)
+                                  for x in batch[:-1]], dtype=np.int32)
+            concat_dev = chainer.dataset.to_device(device, concat)
+            batch_dev = cuda.cupy.split(concat_dev, sections)
+            return batch_dev
+    # ret = to_gpu(ret)
+    with cupy.cuda.Device(device):
+        ret = cupy.array(ret)
     ret = model.translate(ret)
     ret = map(lambda x: x.argmax(), ret.data)
     print(list(ret))
