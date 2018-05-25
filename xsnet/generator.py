cf718ef517ddb18d0306241bbe3fe1dfe2d8522b
PikachuHy <2931408816@qq.com>
2018-05-25 23:36:25 +0800

    fix AttributeError: module 'posixpath' has no attribute 'exist'

--- xsnet/generator.py
+++ xsnet/generator.py
@@ -6,11 +6,11 @@ import mimi
 
 class Mp3Generator(object):
     def generate(self, wav_path, output_path):
-        if not os.path.exist(wav_path):
+        if not os.path.exists(wav_path):
             raise Exception('file: {} not exist'.format(wav_path))
 
         dir_name = os.path.dirname(output_path)
-        if not os.path.exist(dir_name):
+        if not os.path.exists(dir_name):
             os.makedirs(dir_name)
 
         song = AudioSegment.from_wav(wav_path)
@@ -19,11 +19,11 @@ class Mp3Generator(object):
 
 class WavGenerator(object):
     def generate(self, midi_file_path, output_path):
-        if not os.path.exist(midi_file_path):
+        if not os.path.exists(midi_file_path):
             raise Exception('file: {} not exist'.format(midi_file_path))
 
         dir_name = os.path.dirname(output_path)
-        if not os.path.exist(dir_name):
+        if not os.path.exists(dir_name):
             os.makedirs(dir_name)
 
         mimi.output.midi2wav(midi_file_path, output_path)
@@ -31,11 +31,11 @@ class WavGenerator(object):
 
 class MidiGenerator(object):
     def generate(self, midi_txt_file_path, output_path):
-        if not os.path.exist(midi_file_path):
+        if not os.path.exists(midi_txt_file_path):
             raise Exception('file: {} not exist'.format(midi_txt_file_path))
 
         dir_name = os.path.dirname(output_path)
-        if not os.path.exist(dir_name):
+        if not os.path.exists(dir_name):
             os.makedirs(dir_name)
 
         cmd = 't2mf {} {}'.format(midi_txt_file_path, output_path)
